feffdabb7493646d6c88fe78d79acfbe58a9efb6
Leonard W. <pyleow@icloud.com>
2025-06-12 10:47:26 +0800

    Initial commit

---
+++ cbonsai saver/cbonsai saver/cbonsai_saverView.h
@@ -0,0 +1,12 @@
+//
+//  cbonsai_saverView.h
+//  cbonsai saver
+//
+//  Created by Leonard Wang on 2025/6/11.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface cbonsai_saverView : ScreenSaverView
+
+@end
---
+++ cbonsai saver/cbonsai saver/cbonsai_saverView.m
@@ -0,0 +1,51 @@
+//
+//  cbonsai_saverView.m
+//  cbonsai saver
+//
+//  Created by Leonard Wang on 2025/6/11.
+//
+
+#import "cbonsai_saverView.h"
+
+@implementation cbonsai_saverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
